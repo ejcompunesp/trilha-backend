a8bbab924c8f7a915ee255a8ff7af28f59765d0d
Leonardo Yudi Higuti <leonardo.higuti@unesp.br>
2021-05-29 20:28:32 -0300

    feat: init and associate modesl

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,17 @@
 const sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
+const User = require('../models/User');
+const Brand = require('../models/Brand');
+const Product = require('../models/Product');
+
 const connection = new sequelize(dbConfig);
 connection.authenticate()
   .then(() => { console.log('Conexão criada') })
   .catch((error) => { console.log(error) });
+
+User.init(connection);
+Brand.init(connection);
+Product.init(connection);
 
 module.exports = connection;
