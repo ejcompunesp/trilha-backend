568638a996aad0678b682a1c364c23eafe916d1b
Leonardo Yudi Higuti <leonardo.higuti@unesp.br>
2021-05-29 20:28:08 -0300

    fix: env var name

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ module.exports = {
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   define: {
     timestamps: false,
     underscored: true,
