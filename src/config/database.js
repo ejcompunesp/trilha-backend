66c035c185ede4e457754dce44fe5a300534c99e
Leonardo Yudi Higuti <leonardo.higuti@unesp.br>
2021-06-08 17:36:10 -0300

    prepara para deploy

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ module.exports = {
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   define: {
     timestamps: false,
     underscored: true,
