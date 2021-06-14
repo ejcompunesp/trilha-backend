aadb293ec22f82b13a8c1e6015211080fdd590e3
Leonardo Yudi Higuti <leonardo.higuti@unesp.br>
2021-06-14 18:25:08 -0300

    adiciona cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const routes = require('./routes');
 
 require('dotenv').config();
@@ -7,6 +8,7 @@ require('./database');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/images', express.static(path.resolve(__dirname, '..', 'uploads')));
