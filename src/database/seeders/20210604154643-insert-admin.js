3eb35795985b5f67a0ead0a52ced2a6740664a83
Leonardo Yudi Higuti <leonardo.higuti@unesp.br>
2021-06-14 16:59:16 -0300

    fix deploy on heroku

--- src/database/seeders/20210604154643-insert-admin.js
+++ src/database/seeders/20210604154643-insert-admin.js
@@ -4,12 +4,14 @@ const { generateHash } = require("../../utils/auth");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('users', [{
-      name: 'Admin',
-      email: 'admin',
-      password: await generateHash(process.env.ADMIN_PASSWORD),
-      is_admin: true
-    }], {});
+    const users = await queryInterface.sequelize.query('SELECT email FROM users WHERE email = "admin";');
+    if (users[0].length === 0)
+      await queryInterface.bulkInsert('users', [{
+        name: 'Admin',
+        email: 'admin',
+        password: await generateHash(process.env.ADMIN_PASSWORD),
+        is_admin: true
+      }], {});
   },
 
   down: async (queryInterface, Sequelize) => {
